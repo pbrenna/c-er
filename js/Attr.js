c34b6109b63a550dfd13061f8bfc75f31adddbae
Pietro Brenna <pietrobrenna@hotmail.it>
2017-02-27 17:23:45 +0100

    Added license; inkscape tip

--- js/Attr.js
+++ js/Attr.js
@@ -1,5 +1,26 @@
 "use strict";
 
+/*
+ * Attr.js
+ * This file is part of c-er
+ *
+ * Copyright (C) 2017 - Pietro Brenna
+ *
+ * c-er is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * c-er is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with c-er. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 function Attr(node, project) {
     this.node = node
     this.project = project
--- js/Generalization.js
+++ js/Generalization.js
@@ -1,4 +1,24 @@
 "use strict";
+/*
+ * Generalization.js
+ * This file is part of c-er
+ *
+ * Copyright (C) 2017 - Pietro Brenna
+ *
+ * c-er is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * c-er is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with c-er. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 function Generalization(node, project) {
     Concept.apply(this, [node, project])
